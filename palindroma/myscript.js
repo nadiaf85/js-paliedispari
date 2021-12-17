b09f09052f07fe1683d65f773f8c49a6f9d63cde
nadiaf85 <nadia.fiorio@alice.it>
2021-12-17 16:11:10 +0100

    quasi finito pari e dispari

---
+++ palindroma/myscript.js
@@ -0,0 +1,11 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// Chiedere all’utente di inserire una parola
+let parola = prompt('inserisci una parola');
+
+console.log('Anna')
+
+const nomeArray = ['A','N','N','A'];
---
+++ pari e dispari/myscript.js
@@ -0,0 +1,37 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+let pariDispari = prompt('pari o dispari?');
+let numeroUtente = parseInt(prompt('scegli un numero da 1 a 5'));
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+function randomPc(){
+    let numeroRandomPc = (Math.floor(Math.random()*5)+1);
+    return numeroRandomPc;
+}
+
+// Sommiamo i due numeri
+let somma = numeroRandomPc + numeroUtente;
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function sommaNumeri(numeroRandomPc,numeroUtente){
+    let risultato = numeroRandomPc + numeroUtente;
+    return risultato;
+}
+
+//     for(let i=0; i<5; i++){
+//         if(numeroRandomPc %2 == 0){
+//             console.log('ha vinto l utente');
+//         }else{
+//             console.log('ha vinto il pc');
+//         }
+
+// }
+
+    
